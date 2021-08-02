bbb329e530016155f20d50957d44a64e19c50381
up1 <somkiat.p@gmail.com>
2021-08-02 14:00:55 +0700

    Success with fail case

--- demo01/src/main/java/unittest/Hello.java
+++ demo01/src/main/java/unittest/Hello.java
@@ -16,6 +16,11 @@ public class Hello {
 class UserDB { // Connect to DB
     public String getNameById(int id) {
         // Unstable with dependency
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         throw new RuntimeException("DB Fail");
     }
 }
