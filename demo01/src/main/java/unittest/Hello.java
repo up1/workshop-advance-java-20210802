17ba78d6339f83edc87a4edc535083e7e5f965dc
up1 <somkiat.p@gmail.com>
2021-08-02 13:36:59 +0700

    Demo with I = isolate/independent

--- demo01/src/main/java/unittest/Hello.java
+++ demo01/src/main/java/unittest/Hello.java
@@ -4,4 +4,17 @@ public class Hello {
     public String hi(String name) {
         return "Hello, " + name;
     }
+
+    public String workWithDb(int id) {
+        // Tight coupling
+        UserDB userDB = new UserDB();
+        return userDB.getNameById(id);
+    }
+}
+
+class UserDB { // Connect to DB
+    public String getNameById(int id) {
+        // Unstable with dependency
+        throw new RuntimeException("DB Fail");
+    }
 }
--- demo01/src/test/java/unittest/HelloTest.java
+++ demo01/src/test/java/unittest/HelloTest.java
@@ -8,6 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class HelloTest {
 
     @Test
+    @DisplayName("ทำการทดสอบกับ database (I = Isolate/Independent)")
+    public void case02() {
+        Hello hello = new Hello();
+        String name = hello.workWithDb(1);
+        assertEquals("somkiat", name);
+    }
+
+    @Test
     @DisplayName("นี่คือ case แรกของการเขียนนะ")
     public void case01() {
         // Arrange == Given == Pre condition
