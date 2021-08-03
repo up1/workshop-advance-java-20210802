c22f4e50996d8dc0cad9d53ae20e018ad8b3cf58
up1 <somkiat.p@gmail.com>
2021-08-03 13:52:06 +0700

    Invert dependency of Service class

--- demo01/src/main/java/testdouble/GenerateIdService.java
+++ demo01/src/main/java/testdouble/GenerateIdService.java
@@ -3,9 +3,14 @@ package testdouble;
 import java.util.Random;
 
 public class GenerateIdService {
+    // Dependency
+    private Random random = new Random();
+
+    public void setRandom(Random random) {
+        this.random = random;
+    }
 
     public String get() {
-        Random random = new Random(); // Dependency
         int number = random.nextInt(10);
         String id = "XYZ" + number;
         return id;
--- demo01/src/test/java/testdouble/GenerateIdServiceTest.java
+++ demo01/src/test/java/testdouble/GenerateIdServiceTest.java
@@ -3,6 +3,8 @@ package testdouble;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GenerateIdServiceTest {
@@ -18,6 +20,7 @@ public class GenerateIdServiceTest {
     @DisplayName("ID = XYZ7")
     public void case01() {
         GenerateIdService service = new GenerateIdService();
+        service.setRandom(new Random());
         String id = service.get();
         assertEquals("XYZ7", id);
     }
