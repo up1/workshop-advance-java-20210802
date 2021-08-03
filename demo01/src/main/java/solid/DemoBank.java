391c8c73a907a7a1dd00bf05a110b0b298290b1f
up1 <somkiat.p@gmail.com>
2021-08-03 10:33:22 +0700

    Demo with OCP

---
+++ demo01/src/main/java/solid/DemoBank.java
@@ -0,0 +1,56 @@
+package solid;
+
+/**
+ * 1. Transfer money in Thailand only
+ * 2. Transfer money (International)
+ */
+
+public class DemoBank {
+    public static void main(String[] args) {
+        // Tell, Don't Ask
+        // Local
+        TransferMoneyProcess bankAccount = new LocalBankAccount();
+        bankAccount.transferMoney(1000);
+
+        // Inter
+        TransferMoneyProcess interBankAccount = new InterBankAccount();
+        interBankAccount.transferMoney(2000);
+    }
+}
+
+enum AccountType {
+    Local,
+    Inter
+}
+
+interface TransferMoneyProcess {
+    void transferMoney(int amount);
+}
+
+class LocalBankAccount implements  TransferMoneyProcess{
+    public void transferMoney(int amount) {
+        System.out.println("Transfer money in Thailand only");
+        // TODO
+    }
+}
+
+class InterBankAccount implements TransferMoneyProcess {
+    public void transferMoney(int amount) {
+        System.out.println("Transfer money (International)");
+        // TODO
+    }
+}
+
+class InterV2BankAccount implements TransferMoneyProcess {
+    public void transferMoney(int amount) {
+        System.out.println("Transfer money (International V2)");
+        // TODO
+    }
+}
+
+class XXXBankAccount implements TransferMoneyProcess {
+    public void transferMoney(int amount) {
+        System.out.println("Transfer money (XXX)");
+        // TODO
+    }
+}
