ca38b3fdae7d6e3f3ad3aa9fb0ec7518b6418937
up1 <somkiat.p@gmail.com>
2021-08-03 14:45:11 +0700

    Create project with spring boot

---
+++ workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopdip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopDipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopDipApplication.class, args);
+	}
+
+}
---
+++ workshop-dip/src/test/java/com/example/workshopdip/WorkshopDipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopdip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopDipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
