16c1cccc0a0bbdfd1192650c5c2f385a937c2ab6
up1 <somkiat.p@gmail.com>
2021-08-03 15:16:01 +0700

    Use bean from spring boot

--- workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
+++ workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
@@ -1,5 +1,6 @@
 package com.example.workshopdip;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,11 +8,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class WorkshopDipApplication {
 
+	@Autowired
+	private GenerateIdService generateIdService;
+
 	public static void main(String[] args) {
 		// Beans => Java Bean, Enterprise Java Bean (EJB) => SessionBean, JPA
 		ConfigurableApplicationContext context
 				= SpringApplication.run(WorkshopDipApplication.class, args);
 		System.out.println("Bean count=" + context.getBeanDefinitionCount());
+
+		GenerateIdService service
+				= context.getBean(GenerateIdService.class);
+		String id = service.get();
+		System.out.println("ID=" + id);
 	}
 
 }
