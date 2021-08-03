5d87634bcf8fc3dc8522c5eae868486c83fdc9cf
up1 <somkiat.p@gmail.com>
2021-08-03 15:31:43 +0700

    Start with spring boot test

---
+++ workshop-dip/src/test/java/com/example/workshopdip/GenerateIdServiceTest.java
@@ -0,0 +1,23 @@
+package com.example.workshopdip;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class GenerateIdServiceTest {
+
+    @Autowired
+    private GenerateIdService generateIdService;
+
+    @Test
+    @DisplayName("เริ่มต้นทดสอบกับ spting boot test")
+    public void case01(){
+        String id = generateIdService.get();
+        assertEquals("XYZ7", id);
+    }
+
+}
