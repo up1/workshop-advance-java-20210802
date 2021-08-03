53cd6f62cc33f31c33220b20ef17e521b8d941b1
up1 <somkiat.p@gmail.com>
2021-08-03 15:02:48 +0700

    Show number of beans from startup process

--- workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
+++ workshop-dip/src/main/java/com/example/workshopdip/WorkshopDipApplication.java
@@ -2,12 +2,16 @@ package com.example.workshopdip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WorkshopDipApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopDipApplication.class, args);
+		// Beans => Java Bean, Enterprise Java Bean (EJB) => SessionBean, JPA
+		ConfigurableApplicationContext context
+				= SpringApplication.run(WorkshopDipApplication.class, args);
+		System.out.println("Bean count=" + context.getBeanDefinitionCount());
 	}
 
 }
